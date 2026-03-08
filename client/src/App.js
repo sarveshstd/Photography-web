ab1ed961fca21847b5f18545e7a45384c27af38f
Sarveshwaran S <sarveshwaransanjeevi@gmail.com>
2026-03-08 14:20:25 +0530

    Configure for GitHub Pages deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import StudentLogin from './pages/StudentLogin';
 import CoordinatorLogin from './pages/CoordinatorLogin';
 import StudentGallery from './pages/StudentGallery';
