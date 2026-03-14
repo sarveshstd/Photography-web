7344381b028d3cf7c0e4bc6ffe89b3daa7362101
Sarveshwaran S <sarveshwaransanjeevi@gmail.com>
2026-03-14 14:28:55 +0530

    Add cloudinary env diagnostics

--- server/routes/media.js
+++ server/routes/media.js
@@ -8,6 +8,11 @@ const Media = require("../models/Media");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
+console.log("--- CLOUDINARY ENV DIAGNOSTICS ---");
+console.log("Cloud Name exists:", !!process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key exists:", !!process.env.CLOUDINARY_API_KEY);
+console.log("API Secret exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
